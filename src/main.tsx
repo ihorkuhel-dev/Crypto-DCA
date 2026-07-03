4c23c56f4b7e69921eff7fa947cbf65a471402e0
ihorkuhel-dev <ihor.kuhel@innowise.com>
2026-07-03 15:09:22 +0200

    chore: import tailwind into project

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,8 @@ import { createRoot } from 'react-dom/client';
 import { AppProvider } from '@/app/providers/app-provider';
 import { router } from '@/app/router';
 
+import '@/styles/index.css';
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AppProvider>

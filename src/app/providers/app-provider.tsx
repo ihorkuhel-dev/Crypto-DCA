9a90b64c3b217adff7a86e1542eb08fe16dd607c
ihorkuhel-dev <ihor.kuhel@innowise.com>
2026-07-06 14:16:16 +0200

    feat: add theme provider

--- src/app/providers/app-provider.tsx
+++ src/app/providers/app-provider.tsx
@@ -3,14 +3,17 @@ import type {ReactNode} from 'react';
 import {queryClient} from '@/shared/lib/query-client.ts';
 import {ReactQueryDevtools} from '@tanstack/react-query-devtools';
 import {TooltipProvider} from '@/components/ui/tooltip.tsx';
+import {ThemeProvider} from "@/app/providers/theme-provider.tsx";
 
 export function AppProvider({ children }: { children: ReactNode }) {
   return (
-    <TooltipProvider>
-      <QueryClientProvider client={queryClient}>
-        {children}
-        {import.meta.env.DEV && <ReactQueryDevtools initialIsOpen={false} />}
-      </QueryClientProvider>
-    </TooltipProvider>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <TooltipProvider>
+        <QueryClientProvider client={queryClient}>
+          {children}
+          {import.meta.env.DEV && <ReactQueryDevtools initialIsOpen={false} />}
+        </QueryClientProvider>
+      </TooltipProvider>
+    </ThemeProvider>
   );
 }

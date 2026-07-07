287651ad2ab7ea6786ede701787ed030bae86409
ihorkuhel-dev <ihor.kuhel@innowise.com>
2026-07-07 16:43:25 +0200

    feat: implement base app layout

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,10 +1,11 @@
 export function Footer() {
   return (
-    <>
-      <footer className="w-full  p-4 flex gap-4 bg-secondary text-muted-foreground">
-        {/*<p>@Ihor Kuhel</p>*/}
-        {/*<a href="">Github</a>*/}
-      </footer>
-    </>
+    <footer className="w-full  py-4 flex gap-4 border-t  text-muted-foreground text-xs">
+      <p>@Ihor Kuhel</p>
+      <a href="https://github.com/qwerty0zero" target="_blank">
+        Github
+      </a>
+      <p className="ml-auto">@2026</p>
+    </footer>
   );
 }
